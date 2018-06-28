2042e73e375784190f2e611b0ee300cd65374339
Daniel Mancia <dmancia@utexas.edu>
2018-06-27 22:57:08 -0700

    Add template

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define INF 0x3f3f3f3f
+#define MOD 1e9 + 7
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+int main() {
+    ios::sync_with_stdio(false); 
+    cin.tie(0);
+    return 0;
+}
